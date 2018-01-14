58917fcb795e6f67d8c1d3e6daa6ff881917144b
Lothar Braun <code@lobraun.de>
2018-01-14 11:31:03 +0100

    Store DNS Answer configuration in own class
    We now have the class DNSAnswerConfig which stores
    which query type needs to be answered with which value

--- core/dns_reply_generators.py
+++ core/dns_reply_generators.py
@@ -1,6 +1,57 @@
 from twisted.names import client, dns, error, server
 
 import re
+import socket
+
+class DNSAnswerConfig:
+    def isValidQueryType(self, query_type):
+        if query_type in dns.QUERY_TYPES.values():
+            return True
+        return False
+
+    def isIPv4Address(self, address):
+        try:
+            socket.inet_pton(socket.AF_INET, address)
+        except Exception as e:
+            return False
+        return True
+       
+    def isIPv6Address(self, address):
+        try:
+            socket.inet_pton(socket.AF_INET6, address)
+        except Exception as e:
+            return False
+        return True
+ 
+    def __init__(self, value):
+        self.value_dict = dict()
+        if isinstance(value, str):
+            # interpret a string value either as an IPv4 address or
+            # an IPv6 address
+            if self.isIPv4Address(value):
+                self.value_dict['A'] = value
+            elif self.isIPv6Address(value):
+                self.value_dict['AAAA'] = value
+            else:
+                raise RuntimeError("DNSAnswerDict: {} is not a valid IP "
+                                   "address".format(value))
+        elif isinstance(value, list):
+            # a list of IPv4 and IPv6 addresses
+            for address in value:
+                if self.isIPv4Address(address):
+                    self.value_dict.setdefault('A', []).append(address)
+                elif self.isIPv6Address(address):
+                    self.value_dict.setdefault('AAAA', []).append(address)
+                else:
+                    raise RuntimeError("DNSAnswerDict: {} is not a valid IP "
+                                       "address".format(address))
+        elif isinstance(value, dict):
+            for qtype in value.keys():
+                if not self.isValidQueryType(qtype):
+                    raise RuntimeError('DNSAnswerDict: Query type "{}" is not a '
+                                      'valid query type.'.format(qtype))
+            self.value_dict = value
+        
 
 class DNSReplyGenerator:
     def __init__(self, config):
